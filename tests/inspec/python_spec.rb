a98a973529f4de43aef51536fffea1cbb159225e
diodonfrost <diodon.frost@diodonfrost.me>
2020-01-10 19:41:47 +0100

    add inspec test

---
+++ tests/inspec/python_spec.rb
@@ -0,0 +1,12 @@
+# Check if python is functionnal
+
+python_path = '/usr/bin/python --version'
+
+control 'java-01' do
+  impact 1.0
+  title 'Python'
+  desc 'Python should be functionnal'
+  describe command(python_path) do
+    its('exit_status') { should eq 0 }
+  end
+end
