9a5a2b49d86f2a0f757dd57966d7159b58f00330
diodonfrost <diodon.frost@diodonfrost.me>
2020-05-08 16:18:52 +0200

    refactoring python_path config

--- tests/inspec/python_spec.rb
+++ tests/inspec/python_spec.rb
@@ -1,12 +1,16 @@
 # Check if python is functionnal
 
-python_path = '/usr/local/bin/python3 --version'
+python_versions = ['3.5', '3.6', '3.7', '3.8']
 
 control 'java-01' do
   impact 1.0
   title 'Python'
   desc 'Python should be functionnal'
-  describe command(python_path) do
-    its('exit_status') { should eq 0 }
+  describe.one do
+    python_versions.each do |python_version|
+      describe command("/usr/local/python#{python_version}/bin/python#{python_version} --version") do
+        its('exit_status') { should eq 0 }
+      end
+    end
   end
 end
